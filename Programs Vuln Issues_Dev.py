e9b772eec5b7c69f3bc0460b718423c7c0808817
xeeker4368 <M4s1c4368>
2018-08-03 10:57:13 -0400

    Loops thru the number of references and gives either the only version vulnerable or a vulnerable range.

--- Programs Vuln Issues_Dev.py
+++ Programs Vuln Issues_Dev.py
@@ -4,7 +4,7 @@ import json
 import textwrap
 import os
 
-
+"Grabs the most recent CVE file and unzips the file."
 Grab_NVD_Vuln_File = "https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.zip"
 wget.download(Grab_NVD_Vuln_File, "c:\TEMP\NVDCVE.json.zip")
 Zipped_NVDCVE_File = zipfile.ZipFile("c:\TEMP\NVDCVE.json.zip", "r")
@@ -12,8 +12,10 @@ Zipped_NVDCVE_File.extractall("c:\TEMP")
 Zipped_NVDCVE_File.close()
 os.remove("c:\TEMP\NVDCVE.json.zip")
 
+
 Security_Products = [u'cisco', u'f5', u'varonis', u'arcsight', u'Qualys']
 
+"Reads the JSON file from the temp directory"
 with open('c:\TEMP\\nvdcve-1.0-recent.json') as NVDCVE_JSON:
     NVDCVE_JSON_Data = json.load(NVDCVE_JSON)
     Vendor_Version_Count = 0
@@ -22,6 +24,7 @@ with open('c:\TEMP\\nvdcve-1.0-recent.json') as NVDCVE_JSON:
     NVDCVE_Vendor_Name = {}
     CVE_Count = 0
     NVDCVE_Row_Count = NVDCVE_JSON_Data[u'CVE_Items'].__len__()
+    "Gets the count of how many CVE's are in the current list"
     for NVDCVE_Current_Count in range(NVDCVE_Row_Count):
         try:
             Vendor_Version_Count = NVDCVE_JSON_Data[u'CVE_Items'][NVDCVE_Current_Count][u'cve'][u'affects'][u'vendor'] \
@@ -32,19 +35,25 @@ with open('c:\TEMP\\nvdcve-1.0-recent.json') as NVDCVE_JSON:
         NVDCVE_Info[NVDCVE_Current_Count] = NVDCVE_JSON_Data[u'CVE_Items'][NVDCVE_Current_Count]
         NVDCVE_Vendor_Data[NVDCVE_Current_Count] = NVDCVE_Info[NVDCVE_Current_Count][u'cve'][u'affects'][u'vendor'] \
             [u'vendor_data']
+        "Retrieves the vendor name"
         try:
             NVDCVE_Vendor_Name[NVDCVE_Current_Count] = NVDCVE_JSON_Data[u'CVE_Items'][NVDCVE_Current_Count][u'cve'] \
             [u'affects'][u'vendor'][u'vendor_data'][0][u'vendor_name']
         except:
             NVDCVE_Vendor_Name[NVDCVE_Current_Count] = ""
         for product_count in range(len(Security_Products)):
+            "Compares the list of security products you have specified about to the vendor name in the CVE document"
             try:
                 if NVDCVE_Vendor_Name[NVDCVE_Current_Count] == Security_Products[product_count]:
                     print "\033[91m" + "\033[1m" + "Vendor: " + "\033[0m" + NVDCVE_Vendor_Name[ \
                         NVDCVE_Current_Count]
+
                     print "\033[1m" + "\033[91m" + "Product: " + "\033[0m" + \
                         NVDCVE_Info[NVDCVE_Current_Count][u'cve'][u'affects'][u'vendor'][u'vendor_data'][0] \
                         [u'product'][u'product_data'][0][u'product_name']
+
+                    print "\033[91m" + "\033[1m" + "Exploitability: " + "\033[0m" + str(
+                        NVDCVE_Info[NVDCVE_Current_Count][u'impact'][u'baseMetricV2'][u'exploitabilityScore'])
 
                     print "\033[91m" + "\033[1m" + "Version: " + "\033[0m" + NVDCVE_Info[NVDCVE_Current_Count][u'cve'] \
                         [u'affects'] \
@@ -54,17 +63,18 @@ with open('c:\TEMP\\nvdcve-1.0-recent.json') as NVDCVE_JSON:
                     print "\033[91m" + "\033[1m" + "Published Date: " + "\033[0m" + NVDCVE_Info[NVDCVE_Current_Count][ \
                         u'publishedDate']
 
-                    print "\033[91m" + "\033[1m" + "Modified Date: " + "\033[0m" + NVDCVE_Info[NVDCVE_Current_Count][ \
+                    print "\033[91m" + "\033[1m" + "Published Date: " + "\033[0m" + NVDCVE_Info[NVDCVE_Current_Count][ \
                         u'lastModifiedDate']
 
                     print "\033[91m" + "\033[1m" + "Description: " + "\033[0m" + (textwrap.fill(NVDCVE_Info
                             [NVDCVE_Current_Count][u'cve'][u'description'][u'description_data']
                             [0][u'value'], 125))
 
-                    print "\033[91m" + "\033[1m" + "Exploitability: " + "\033[0m" + str(
-                        NVDCVE_Info[NVDCVE_Current_Count][u'impact'][u'baseMetricV2'][u'exploitabilityScore'])
+                    print "\033[91m" + "\033[1m" + "Link to Description: " + "\033[0m" + \
+                          NVDCVE_Info[NVDCVE_Current_Count][u'cve'][u'references'][u'reference_data'][0][u'url']
 
                     print ""
+
                     NVDCVE_Current_Count = NVDCVE_Current_Count + 1
                     CVE_Count = CVE_Count + 1
             except:
@@ -73,4 +83,4 @@ with open('c:\TEMP\\nvdcve-1.0-recent.json') as NVDCVE_JSON:
         NVDCVE_Current_Count = NVDCVE_Current_Count + 1
         continue
     print "\033[91m" + "\033[1m" + "Total Number of Exploits Found: " + "\033[0m",CVE_Count
-    exit()
+exit()
